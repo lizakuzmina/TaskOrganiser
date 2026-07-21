77cce9da82fa6ebaee0a588ca86ef7078b6e2e7b
Yelyzaveta Kuzmina <liza.kuzmina26@gmail.com>
2026-07-21 17:13:37 +0300

    Complete TaskViewModel and TaskService CRUD logic
    - Added markCompleted flow
    - Finished TaskViewModel CRUD operations
    - Connected TaskService with TaskStore
    - Completed task management logic

--- Organiser/Oraniser/Oraniser/Sources/ViewModels/TaskViewModel.swift
+++ Organiser/Oraniser/Oraniser/Sources/ViewModels/TaskViewModel.swift
@@ -5,3 +5,61 @@
 //  Created by Yelyzaveta Kuzmina on 17.07.2026.
 //
 
+import Foundation
+import Combine
+
+class TaskViewModel: ObservableObject {
+    init(service: any TaskServicing) {
+        self.service = service
+    }
+    
+    @Published var tasks: [Task] = []
+    @Published var error: Error?
+    
+    private let service: TaskServicing
+    
+    func loadTasks() {
+        do {
+            let loadedTasks = try service.loadTasks()
+            tasks = loadedTasks
+        } catch {
+            self.error = error
+        }
+    }
+    
+    func addTask (_ task: Task) {
+        do {
+            try service.addTask(task)
+            loadTasks()
+        } catch {
+            self.error = error
+        }
+    }
+    
+    func deleteTask (id: UUID) {
+        do {
+            try service.deleteTask(id: id)
+            loadTasks()
+        } catch {
+            self.error = error
+        }
+    }
+    
+    func updateTask () {
+        do {
+            try service.updateTask()
+            loadTasks()
+        } catch {
+            self.error = error
+        }
+    }
+    
+    func markCompleted(id: UUID) {
+        do {
+            try service.markCompleted(id: id)
+            loadTasks()
+        } catch {
+            self.error = error
+        }
+    }
+}
