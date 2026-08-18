fd6538c51a958e209464785ca88f057e7ab73eab
Yelyzaveta Kuzmina <liza.kuzmina26@gmail.com>
2026-08-18 16:27:02 +0300

    create TaskListView

--- Organiser/Oraniser/Oraniser/Sources/Views/TaskListView.swift
+++ Organiser/Oraniser/Oraniser/Sources/Views/TaskListView.swift
@@ -8,7 +8,19 @@
 import SwiftUI
 
 struct TaskListView: View {
+    
+    @ObservedObject var viewModel: TaskViewModel
+    
     var body: some View {
+        ScrollView {
+            LazyVStack {
+                ForEach(viewModel.tasks) { task in
+                    TaskCardView(task: task)
+                }
+            }.onAppear {
+                viewModel.loadTasks()
+            }
+        }
     }
 }
 //
